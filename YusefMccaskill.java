e50b51e6be67acd11197b962482b15a1c1809f4e
Yusef McCaskill <yusef.mccaskill@mrjaffesclass.com>
2023-01-09 14:12:56 -0800

    intial commit

---
+++ YusefMccaskill.java
@@ -0,0 +1,29 @@
+
+/**
+ * A BoopleSnoot is Mr. Jaffe's animal
+ * 
+ * @author Mr. Jaffe
+ * @version 1.0 2017-07-14
+ */
+public class YusefMccaskill extends Animal
+{
+  public YusefMccaskill() {
+    super("Yusef Mccaskill", "orange", 4);
+  }
+  
+  public String speak() {
+    return "Roar";
+  }
+  
+  public String getDisposition() {
+    return "Angry";
+  }
+  
+  public String getType() {
+    return "Lion";
+  }
+  
+  public String getName() {
+    return "Jaffe";
+  }
+}
